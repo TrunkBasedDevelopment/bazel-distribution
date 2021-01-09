3088b69bb95f0c222d1fd50ac3efc8e464d99c30
Paul Hammant <paul@hammant.org>
2021-01-09 15:55:07 +0000

    start of work on #277

--- maven/templates/deploy.py
+++ maven/templates/deploy.py
@@ -25,6 +25,7 @@ from xml.etree import ElementTree
 import hashlib
 import os
 import re
+import requests
 import subprocess as sp
 import sys
 import tempfile
@@ -52,6 +53,16 @@ def upload(url, username, password, local_fn, remote_fn):
         raise Exception('upload of {} failed, got HTTP status code {}'.format(
             local_fn, upload_status_code))
 
+
+def download(url, username, password, remote_fn):
+
+    r = requests.get(urljoin(url, remote_fn), auth=(username, password))
+    if r.status_code == '404':
+        return None
+    if r.status_code != '200':
+        raise Exception('download of {} failed, got HTTP status code {}'.format(
+            remote_fn, upload_status_code))
+    return r.text
 
 def sign(fn):
     # TODO(vmax): current limitation of this functionality
@@ -130,13 +141,28 @@ if repo_type == 'release' and len(re.findall(version_release_regex, version)) ==
 filename_base = '{coordinates}/{artifact}/{version}/{artifact}-{version}'.format(
     coordinates=group_id.text.replace('.', '/'), version=version, artifact=artifact_id.text)
 
-upload(maven_url, username, password, pom_file_path, filename_base + '.pom')
-if should_sign:
-    upload(maven_url, username, password, sign(pom_file_path), filename_base + '.pom.asc')
-upload(maven_url, username, password, jar_path, filename_base + '.jar')
-if should_sign:
-    upload(maven_url, username, password, sign(jar_path), filename_base + '.jar.asc')
+sha_of_jar = sha1(jar_path)
+sha_of_pom = sha1(pom_file_path)
+sha_of_jar_already_published = download(maven_url, username, password, filename_base + '.jar.sha1')
+sha_of_pom_already_published = download(maven_url, username, password, filename_base + '.pom.sha1')
 if os.path.exists(srcjar_path):
+    sha_of_source_jar = sha1(srcjar_path)
+else:
+    sha_of_source_jar = None
+sha_of_source_jar_already_published = download(maven_url, username, password, filename_base + '-sources.jar.sha1')
+
+if sha_of_pom_already_published != sha_of_pom:
+    upload(maven_url, username, password, pom_file_path, filename_base + '.pom')
+    if should_sign:
+        upload(maven_url, username, password, sign(pom_file_path), filename_base + '.pom.asc')
+
+if sha_of_jar_already_published != sha_of_jar:
+    upload(maven_url, username, password, jar_path, filename_base + '.jar')
+    if should_sign:
+        upload(maven_url, username, password, sign(jar_path), filename_base + '.jar.asc')
+
+if os.path.exists(srcjar_path) and sha_of_source_jar_already_published != sha_of_source_jar:
+
     upload(maven_url, username, password, srcjar_path, filename_base + '-sources.jar')
     if should_sign:
         upload(maven_url, username, password, sign(srcjar_path), filename_base + '-sources.jar.asc')
@@ -145,27 +171,31 @@ if os.path.exists(srcjar_path):
     if should_sign:
         upload(maven_url, username, password, sign(srcjar_path), filename_base + '-javadoc.jar.asc')
 
-with tempfile.NamedTemporaryFile(mode='wt', delete=True) as pom_md5:
-    pom_md5.write(md5(pom_file_path))
-    pom_md5.flush()
-    upload(maven_url, username, password, pom_md5.name, filename_base + '.pom.md5')
+if sha_of_pom_already_published != sha_of_pom:
+    with tempfile.NamedTemporaryFile(mode='wt', delete=True) as pom_md5:
+        pom_md5.write(md5(pom_file_path))
+        pom_md5.flush()
+        upload(maven_url, username, password, pom_md5.name, filename_base + '.pom.md5')
 
-with tempfile.NamedTemporaryFile(mode='wt', delete=True) as pom_sha1:
-    pom_sha1.write(sha1(pom_file_path))
-    pom_sha1.flush()
-    upload(maven_url, username, password, pom_sha1.name, filename_base + '.pom.sha1')
+    with tempfile.NamedTemporaryFile(mode='wt', delete=True) as pom_sha1:
+        pom_sha1.write(sha_of_pom)
+        pom_sha1.flush()
+        upload(maven_url, username, password, pom_sha1.name, filename_base + '.pom.sha1')
 
-with tempfile.NamedTemporaryFile(mode='wt', delete=True) as jar_md5:
-    jar_md5.write(md5(jar_path))
-    jar_md5.flush()
-    upload(maven_url, username, password, jar_md5.name, filename_base + '.jar.md5')
 
-with tempfile.NamedTemporaryFile(mode='wt', delete=True) as jar_sha1:
-    jar_sha1.write(sha1(jar_path))
-    jar_sha1.flush()
-    upload(maven_url, username, password, jar_sha1.name, filename_base + '.jar.sha1')
+if sha_of_jar_already_published != sha_of_jar:
+    with tempfile.NamedTemporaryFile(mode='wt', delete=True) as jar_md5:
+        jar_md5.write(md5(jar_path))
+        jar_md5.flush()
+        upload(maven_url, username, password, jar_md5.name, filename_base + '.jar.md5')
 
-if os.path.exists(srcjar_path):
+    with tempfile.NamedTemporaryFile(mode='wt', delete=True) as jar_sha1:
+        jar_sha1.write(sha_of_jar)
+        jar_sha1.flush()
+        upload(maven_url, username, password, jar_sha1.name, filename_base + '.jar.sha1')
+
+if os.path.exists(srcjar_path) and sha_of_source_jar_already_published != sha_of_source_jar:
+
     with tempfile.NamedTemporaryFile(mode='wt', delete=True) as srcjar_md5:
         srcjar_md5.write(md5(srcjar_path))
         srcjar_md5.flush()
@@ -174,7 +204,7 @@ if os.path.exists(srcjar_path):
         upload(maven_url, username, password, srcjar_md5.name, filename_base + '-javadoc.jar.md5')
 
     with tempfile.NamedTemporaryFile(mode='wt', delete=True) as srcjar_sha1:
-        srcjar_sha1.write(sha1(srcjar_path))
+        srcjar_sha1.write(sha_of_source_jar)
         srcjar_sha1.flush()
         upload(maven_url, username, password, srcjar_sha1.name, filename_base + '-sources.jar.sha1')
         # TODO(vmax): use checksum of real Javadoc instead of srcjar
